a33e33a84b388bd8d712ce88c771eaabed6cdd66
Dominika <dominikapiganova@mail.ru>
2021-08-04 09:55:22 +0300

    Initial Commit

---
+++ beer_bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beer_bar
+//
+//  Created by Dominika Poleshyck on 4.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
